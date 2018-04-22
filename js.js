1cabb8df8c0a94e087097902fdb92de2b852d72d
pandacoder90 <32179994+pandacoder90@users.noreply.github.com>
2018-04-21 20:49:41 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+alert("Welcome");
+var Name = prompt("How's it going, what's your name");
+var Grade = prompt("What's your grade?");
+document.write("Hello, " + Name + ". Welcome to Battle Learn! Click " +
+"Start to Begin! We set your grade level to " + Grade +  ".");
+console.log("Program Completed");
+function myFunction() {
+  window.open("https://scratch.mit.edu/projects/217866361/");
+}
